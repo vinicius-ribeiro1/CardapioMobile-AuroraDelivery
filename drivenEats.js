092672b981391d4e9b6b64cdd991746964a1a3bd
Vinicius <viniciusrdias@hotmail.com>
2026-06-16 15:11:19 -0300

    Seleção de itens + Botao de finalizar pedidos

--- drivenEats.js
+++ drivenEats.js
@@ -4,6 +4,7 @@ function toggleComida(comidaEscolhida) {
     let refSelecionada = document.querySelector(".selecionado");
     if (refSelecionada !== null) {
         refSelecionada.classList.toggle("selecionado");
+        itensSelecionados = itensSelecionados - 1;
     }
     comidaEscolhida.classList.add("selecionado");
     itensSelecionados = itensSelecionados + 1;
@@ -14,6 +15,7 @@ function toggleBebida(bebidaEscolhida) {
     let drinkSelecionada = document.querySelector(".selecionada");
     if (drinkSelecionada !== null) {
         drinkSelecionada.classList.toggle("selecionada");
+        itensSelecionados = itensSelecionados - 1;
     }
     bebidaEscolhida.classList.add("selecionada");
     itensSelecionados = itensSelecionados + 1;
@@ -24,16 +26,37 @@ function toggleSobremesa(sobremesaEscolhida) {
     let desertSelecionada = document.querySelector(".selecionadoo");
     if (desertSelecionada !== null) {
         desertSelecionada.classList.toggle("selecionadoo");
+        itensSelecionados = itensSelecionados - 1;
     }
     sobremesaEscolhida.classList.add("selecionadoo");
     itensSelecionados = itensSelecionados + 1;
-    fecharPedido(itensSelecionados);
+    fecharPedido(itensSelecionados);  
 }
-
 
 function fecharPedido(itensSelecionados) {
     if (itensSelecionados === 3) {
-        let verde = document.querySelector(".verde");
-        verde.innerHTML = ("Fechar Pedido")
+        let verde = document.querySelector(".botaoFecharPedido");
+        verde.innerHTML = ("Fechar Pedido");
+        verde.classList.add("verdeFundo");
     }
+}
+
+function confirmarPedido() {
+    const comida = document.querySelector(".selecionado");
+    const bebida = document.querySelector(".selecionada");
+    const sobremesa = document.querySelector(".selecionadoo");
+
+    const nomeComida = comida.querySelector("h3").innerText;
+    const precoComida = comida.querySelector(".preco").innerText;
+
+    const nomeBebida = bebida.querySelector("h3").innerText;
+    const precoBebida = bebida.querySelector(".preco").innerText;
+
+    const nomeSobremesa = sobremesa.querySelector("h3").innerText;
+    const precoSobremesa = sobremesa.querySelector(".preco").innerText;
+
+    
+    console.log(nomeComida, precoComida);
+    console.log(nomeBebida, precoBebida);
+    console.log(nomeSobremesa, precoSobremesa);
 }
