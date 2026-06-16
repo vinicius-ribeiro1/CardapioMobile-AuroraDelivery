40e6c9a79f95002a4a6d7e0393e16db670e4d45b
Vinicius <viniciusrdias@hotmail.com>
2026-06-16 17:19:51 -0300

    Quase finalizado, faltando check + Bônus

--- drivenEats.js
+++ drivenEats.js
@@ -41,22 +41,32 @@ function fecharPedido(itensSelecionados) {
     }
 }
 
+
 function confirmarPedido() {
     const comida = document.querySelector(".selecionado");
     const bebida = document.querySelector(".selecionada");
     const sobremesa = document.querySelector(".selecionadoo");
 
-    const nomeComida = comida.querySelector("h3").innerText;
-    const precoComida = comida.querySelector(".preco").innerText;
+    const nomeComida = comida.querySelector("h3").innerHTML;
+    const precoComida = parseInt(comida.querySelector(".preco").innerHTML);
 
-    const nomeBebida = bebida.querySelector("h3").innerText;
-    const precoBebida = bebida.querySelector(".preco").innerText;
+    const nomeBebida = bebida.querySelector("h3").innerHTML;
+    const precoBebida = parseInt(bebida.querySelector(".preco").innerHTML);
 
-    const nomeSobremesa = sobremesa.querySelector("h3").innerText;
-    const precoSobremesa = sobremesa.querySelector(".preco").innerText;
+    const nomeSobremesa = sobremesa.querySelector("h3").innerHTML;
+    const precoSobremesa = parseInt(sobremesa.querySelector(".preco").innerHTML);
 
-    
-    console.log(nomeComida, precoComida);
-    console.log(nomeBebida, precoBebida);
-    console.log(nomeSobremesa, precoSobremesa);
+    const total = precoComida + precoBebida + precoSobremesa;
+
+    const mensagem = `Olá, gostaria de fazer o pedido:
+
+- Prato: ${nomeComida}
+- Bebida: ${nomeBebida}
+- Sobremesa: ${nomeSobremesa}
+
+Total: R$ ${total.toFixed(2)}`;
+
+    const url = `https://wa.me/5522999372504?text=${encodeURIComponent(mensagem)}`;
+
+    window.open(url);
 }
